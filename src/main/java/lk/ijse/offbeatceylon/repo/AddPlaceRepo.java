2b2e2272fedd581acf9498a9f50bcf834202357e
Shimara-Appuhami <shimarailshani@gmail.com>
2025-03-17 22:24:56 +0530

    addPlace repo

--- src/main/java/lk/ijse/offbeatceylon/repo/AddPlaceRepo.java
+++ src/main/java/lk/ijse/offbeatceylon/repo/AddPlaceRepo.java
@@ -3,5 +3,6 @@ package lk.ijse.offbeatceylon.repo;
 import lk.ijse.offbeatceylon.entity.AddPlaces;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AddPlaceRepo extends JpaRepository<AddPlaces,Integer> {
+public interface AddPlaceRepo extends JpaRepository<AddPlaces,Long> {
+    boolean existsByPlaceName(String placeName);
 }
