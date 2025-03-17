8afcddf9ece53bebcbba1f6fa07bf0cff3974788
Shimara-Appuhami <shimarailshani@gmail.com>
2025-03-17 22:24:38 +0530

    addPlace service implement

--- src/main/java/lk/ijse/offbeatceylon/service/impl/AddPlaceServiceImpl.java
+++ src/main/java/lk/ijse/offbeatceylon/service/impl/AddPlaceServiceImpl.java
@@ -6,28 +6,23 @@ import lk.ijse.offbeatceylon.repo.AddPlaceRepo;
 import lk.ijse.offbeatceylon.service.AddPlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
+@Transactional
 public class AddPlaceServiceImpl implements AddPlaceService {
+
     @Autowired
     private AddPlaceRepo addPlaceRepo;
 
     @Override
-    public void addPlace(AddPlaceDTO addPlaceDTO) {
-        // Convert DTO to Entity
-        AddPlaces place = new AddPlaces();
-        place.setPlaceName(addPlaceDTO.getPlaceName());
-        place.setAboutPlace(addPlaceDTO.getAboutPlace());
-        place.setDistrict(addPlaceDTO.getDistrict());
-        place.setImages(addPlaceDTO.getImages());
-        place.setStatus(addPlaceDTO.getStatus());
-        place.setLocation(addPlaceDTO.getLocation());
+    public AddPlaces savePlace(AddPlaces place) {
+        return addPlaceRepo.save(place);
+    }
 
-        // Save to database
-        addPlaceRepo.save(place);
-
-        System.out.println("Place added successfully: " + addPlaceDTO);
+    @Override
+    public boolean existsByPlaceName(String placeName) {
+        return addPlaceRepo.existsByPlaceName(placeName);
     }
 }
