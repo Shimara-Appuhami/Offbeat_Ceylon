394f3c43bb9f37049194050804a2308cfc5d2b87
Shimara-Appuhami <shimarailshani@gmail.com>
2025-03-17 22:25:54 +0530

    fix addPlace image

---
+++ src/main/java/lk/ijse/offbeatceylon/img/FileUploadUtil.java
@@ -0,0 +1,32 @@
+package lk.ijse.offbeatceylon.img;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+
+public class FileUploadUtil {
+    public  static  void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
+
+        Path uploadPath = Paths.get("src/main/resources/static/" + uploadDir);
+
+        // Create directories if they don't exist
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Saved file: " + filePath);
+        } catch (IOException ex) {
+            throw new IOException("Could not save file: " + fileName, ex);
+        }
+    }
+
+}
---
+++ src/main/java/lk/ijse/offbeatceylon/img/ImageUploadController.java
@@ -0,0 +1,43 @@
+package lk.ijse.offbeatceylon.img;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
+
+@RequestMapping("api/v1/img")
+@RestController
+public class ImageUploadController {
+
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> saveImage(@RequestParam("files") MultipartFile[] files) {
+
+        String uploadDir = "imageFolder";
+
+        try {
+            Arrays.asList(files).forEach(file -> {
+                String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+                System.out.println("Uploading file: " + fileName);
+                try {
+                    FileUploadUtil.saveFile(uploadDir, fileName, file);
+                } catch (IOException e) {
+                    throw new RuntimeException("Failed to save file: " + fileName, e);
+                }
+            });
+            return ResponseEntity.ok("Images uploaded successfully!");
+        } catch (RuntimeException e) {
+            System.err.println("Error while uploading: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload images!");
+        }
+    }
+
+
+
+}
