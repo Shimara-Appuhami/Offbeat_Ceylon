46cdef5774c4fc9f2b868763ee30bdee65e86a7a
Shimara-Appuhami <shimarailshani@gmail.com>
2025-04-20 21:52:56 +0530

    add places

--- Front-End/js/dashboard.js
+++ Front-End/js/dashboard.js
@@ -78,3 +78,4 @@ const today = new Date();
 const options = { year: 'numeric', month: 'long', day: 'numeric' };
 document.getElementById('inline-date').textContent = today.toLocaleDateString(undefined, options);
 
+
